f44e5401425f648ebfea584c6dc1a26f5e44f8b1
Romain Beaumont <r.beaumont@criteo.com>
2021-08-02 15:45:06 +0200

    update authors in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -19,8 +19,8 @@ sys.path.insert(0, os.path.abspath(".."))
 # -- Project information -----------------------------------------------------
 
 project = "autofaiss"
-copyright = "2020, Reco.Modules"
-author = "Reco.Modules"
+copyright = "2020, Criteo"
+author = "Criteo reco team"
 
 # The full version, including alpha/beta/rc tags
 release = "1.0.0"
