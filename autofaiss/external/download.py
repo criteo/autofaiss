995302fe73ab13c55eb70c3521c1771a542f7f9a
Romain Beaumont <romain.rom1@gmail.com>
2021-08-04 16:33:24 +0000

    Add keys files in download
    Simple way to keep track of embeddings

--- autofaiss/external/download.py
+++ autofaiss/external/download.py
@@ -14,15 +14,18 @@ from autofaiss.utils.decorators import Timeit
 def download_from_hdfs(
     embeddings_hdfs_path: str,
     local_save_path: str,
-    n_cores: int = 44,
+    n_cores: int = 10,
     delete_tmp_files: bool = True,
     bandwidth_gbytes_per_sec: Optional[float] = None,
     verbose: bool = True,
     embedding_column_name: str = "embedding",
+    keys_folder: Optional[str] = None,
+    key_column_name: Optional[str] = None,
 ) -> None:
     """
     Download a file containing embeddings in parquet format on hdfs and
     convert it to several .npy files
+    Optionally save keys in another folder (useful to keep track of what is each embedding)
     """
     if bandwidth_gbytes_per_sec is not None:
         infos, _ = get_estimated_download_time_infos(embeddings_hdfs_path, bandwidth_gbytes_per_sec)
@@ -33,7 +36,13 @@ def download_from_hdfs(
 
     with Timeit("Convert .parquet files to numpy arrays", indent=1):
         convert_all_parquet_to_numpy(
-            local_save_path, local_save_path, delete=delete_tmp_files, embedding_column_name=embedding_column_name
+            local_save_path,
+            local_save_path,
+            delete=delete_tmp_files,
+            embedding_column_name=embedding_column_name,
+            n_cores=n_cores,
+            keys_folder=keys_folder,
+            key_column_name=key_column_name,
         )
 
 
