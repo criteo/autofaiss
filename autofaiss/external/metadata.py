6b08722890c8d0dea3b224abd587d4921ef809c4
v.paltz <v.paltz@criteo.com>
2021-08-06 23:13:38 +0200

    Add detailed description for the indices part 1

--- autofaiss/external/metadata.py
+++ autofaiss/external/metadata.py
@@ -7,20 +7,28 @@ from enum import Enum
 from math import ceil, log2
 
 from autofaiss.utils.cast import cast_bytes_to_memory_string
+from autofaiss.external.descriptions import (
+    INDEX_DESCRIPTION_BLOCKS,
+    IndexBlock,
+    TUNABLE_PARAMETERS_DESCRIPTION_BLOCKS,
+    TunableParam,
+)
 
 
 class IndexType(Enum):
     FLAT = 0
     HNSW = 1
-    OPQ_IVF = 2
-    OPQ_IVF_HNSW = 3
-    PAD_IVF_HNSW = 4
+    OPQ_IVF_PQ = 2
+    OPQ_IVF_HNSW_PQ = 3
+    PAD_IVF_HNSW_PQ = 4
     NOT_SUPPORTED = 5
 
 
-class Metadata:
+class IndexMetadata:
     """
     Class to compute index metadata given the index_key, the number of vectors and their dimension.
+
+    Note: We don't create classes for each index type in order to keep the code simple.
     """
 
     def __init__(self, index_key: str, nb_vectors: int, dim_vector: int):
@@ -28,12 +36,19 @@ class Metadata:
         self.index_key = index_key
         self.nb_vectors = nb_vectors
         self.dim_vector = dim_vector
+        self.fast_description = ""
+        self.description_blocs = []
+        self.tunable_params = []
         self.params = {}
 
         params = [int(x) for x in re.findall(r"\d+", index_key)]
 
         if any(re.findall(r"OPQ\d+_\d+,IVF\d+,PQ\d+", index_key)):
-            self.index_type = IndexType.OPQ_IVF
+            self.index_type = IndexType.OPQ_IVF_PQ
+            self.fast_description = "An inverted file index (IVF) with quantization and OPQ preprocessing."
+            self.description_blocs = [IndexBlock.IVF, IndexBlock.PQ, IndexBlock.OPQ]
+            self.tunable_params = [TunableParam.NPROBE, TunableParam.HT]
+
             self.params["pq"] = params[3]
             self.params["nbits"] = params[4] if len(params) == 5 else 8  # default value
             self.params["ncentroids"] = params[2]
@@ -41,7 +56,11 @@ class Metadata:
             self.params["M_OPQ"] = params[0]
 
         elif any(re.findall(r"OPQ\d+_\d+,IVF\d+_HNSW\d+,PQ\d+", index_key)):
-            self.index_type = IndexType.OPQ_IVF_HNSW
+            self.index_type = IndexType.OPQ_IVF_HNSW_PQ
+            self.fast_description = "An inverted file index (IVF) with quantization, OPQ preprocessing, and HNSW index."
+            self.description_blocs = [IndexBlock.IVF_HNSW, IndexBlock.HNSW, IndexBlock.PQ, IndexBlock.OPQ]
+            self.tunable_params = [TunableParam.NPROBE, TunableParam.EFSEARCH, TunableParam.HT]
+
             self.params["M_HNSW"] = params[3]
             self.params["pq"] = params[4]
             self.params["nbits"] = params[5] if len(params) == 6 else 8  # default value
@@ -50,7 +69,13 @@ class Metadata:
             self.params["M_OPQ"] = params[0]
 
         elif any(re.findall(r"Pad\d+,IVF\d+_HNSW\d+,PQ\d+", index_key)):
-            self.index_type = IndexType.PAD_IVF_HNSW
+            self.index_type = IndexType.PAD_IVF_HNSW_PQ
+            self.fast_description = (
+                "An inverted file index (IVF) with quantization, a padding on input vectors, and HNSW index."
+            )
+            self.description_blocs = [IndexBlock.IVF_HNSW, IndexBlock.HNSW, IndexBlock.PQ, IndexBlock.PAD]
+            self.tunable_params = [TunableParam.NPROBE, TunableParam.EFSEARCH, TunableParam.HT]
+
             self.params["out_d"] = params[0]
             self.params["M_HNSW"] = params[2]
             self.params["pq"] = params[3]
@@ -59,13 +84,28 @@ class Metadata:
 
         elif any(re.findall(r"HNSW\d+", index_key)):
             self.index_type = IndexType.HNSW
+            self.fast_description = "An HNSW index."
+            self.description_blocs = [IndexBlock.HNSW]
+            self.tunable_params = [TunableParam.EFSEARCH]
+
             self.params["M_HNSW"] = params[0]
+
         elif index_key == "Flat":
             self.index_type = IndexType.FLAT
+            self.fast_description = "A simple flat index."
+            self.description_blocs = [IndexBlock.FLAT]
+            self.tunable_params = []
+
         else:
             self.index_type = IndexType.NOT_SUPPORTED
+            self.fast_description = "No description for this index, feel free to contribute :)"
+            self.description_blocs = []
+            self.tunable_params = []
 
     def get_index_type(self) -> IndexType:
+        """
+        return the index type.
+        """
         return self.index_type
 
     def estimated_index_size_in_bytes(self) -> int:
@@ -82,7 +122,7 @@ class Metadata:
             vectors_size_in_bytes = self.nb_vectors * self.dim_vector * 4
             return vectors_size_in_bytes + hnsw_graph_in_bytes
 
-        if self.index_type in [IndexType.OPQ_IVF, IndexType.OPQ_IVF_HNSW, IndexType.PAD_IVF_HNSW]:
+        if self.index_type in [IndexType.OPQ_IVF_PQ, IndexType.OPQ_IVF_HNSW_PQ, IndexType.PAD_IVF_HNSW_PQ]:
             # We neglict the size of the OPQ table for the moment.
             code_size = ceil(self.params["pq"] * self.params["nbits"] / 8)
             cluster_size_byte = 1 + int((log2(self.params["ncentroids"]) - 1) // 8)
@@ -91,28 +131,53 @@ class Metadata:
             vectors_size_in_bytes = self.nb_vectors * vector_size_byte
             centroid_size_in_bytes = self.params["ncentroids"] * self.dim_vector * 4
 
+            # TODO compute the size of the OPQ table and PAD for the size estimation
+
             total_size_in_byte = vectors_size_in_bytes + centroid_size_in_bytes
 
-            if self.index_type in [IndexType.OPQ_IVF_HNSW, IndexType.PAD_IVF_HNSW]:
+            if self.index_type in [IndexType.OPQ_IVF_HNSW_PQ, IndexType.PAD_IVF_HNSW_PQ]:
                 total_size_in_byte += self.params["ncentroids"] * self.params["M_HNSW"] * 2 * 4
 
             return total_size_in_byte
 
         return -1
 
-    def get_index_description(self) -> str:
+    def get_index_description(self, tunable_parameters_infos=False) -> str:
         """
-        Give a description of the index.
+        Gives a generic description of the index.
         """
 
         description = ""
 
-        if self.index_type == IndexType.FLAT:
-            description += "An index that stores all the vectors in a simple array without any compression\n"
-        else:
-            description += "#TODO"
+        description += self.fast_description + "\n\n"
+        description += "\n".join(INDEX_DESCRIPTION_BLOCKS[desc] for desc in self.description_blocs) + "\n\n"
+
+        if tunable_parameters_infos:
+            if not self.tunable_params:
+                description += "No parameters can be tuned to find a query speed VS recall tradeoff\n\n"
+            else:
+                description += "List of parameters that can be tuned to find a query speed VS recall tradeoff:"
+                description += (
+                    "\n".join(TUNABLE_PARAMETERS_DESCRIPTION_BLOCKS[desc] for desc in self.tunable_params) + "\n\n"
+                )
 
         index_size_string = cast_bytes_to_memory_string(self.estimated_index_size_in_bytes())
-        description += f"The size of the index should be around {index_size_string}."
+        description += f"The size of the index should be around {index_size_string}.\n\n"
+        description += """
+For all indices except the flat index, the query speed can be ajusted.
+The lower the speed limit the lower the recall. With a looser constraint
+on the query time, the recall can be higher, but it is limited by the index
+structure (if there is quantization for instance).
+"""
+        return description
+
+    def get_index_detailed_description(self) -> str:
+        """
+        Give a detailed description of the index.
+        """
+
+        # TODO add a detailed description for the indices.
+
+        description = "TODO"
 
         return description
