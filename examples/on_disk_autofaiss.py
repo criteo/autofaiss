17dd237b527423896252af386b9ba729c42e5e86
Bokai Yu <b.yu@criteo.com>
2022-03-11 21:29:19 +0100

    Fix the example in on_disk_autofaiss.py

--- examples/on_disk_autofaiss.py
+++ examples/on_disk_autofaiss.py
@@ -5,5 +5,5 @@ build_index(
     index_path="knn.index",
     index_infos_path="infos.json",
     max_index_memory_usage="4G",
-    current_memory_available="4G",
+    current_memory_available="5G",
 )
